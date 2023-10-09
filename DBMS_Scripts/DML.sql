8c7085ae46e0433ba4e393511d002a227ec3c051
Praduman <praduman.kachhwaha@kalvium.community>
2023-10-09 19:40:44 +0530

    added DML commands

--- DBMS_Scripts/DML.sql
+++ DBMS_Scripts/DML.sql
@@ -1,2 +1,5 @@
 INSERT INTO users (username, email, password, createdAt, updatedAt) VALUES ('Rahul', 'rahul@gmail.com', 'password@123', NOW(), NOW());
 
+SELECT * FROM restaurant;
+
+INSERT INTO reservations (dateofbook, customer_id, reservation_id) VALUES ('2021-01-01', 1, 1);
