ee9ca58940ff184e7cb32d2c78c3cd7f4938bb73
Praduman <praduman.kachhwaha@kalvium.community>
2023-10-09 11:27:07 +0530

    added DML and DDL command

---
+++ DBMS_Scripts/DDL.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE restaurant (
+ restaurant_id INT PRIMARY KEY,
+ name VARCHAR(50) NOT NULL,
+ address VARCHAR(100) NOT NULL,
+ phone_nunmber INT NOT NULL
+)
+
---
+++ DBMS_Scripts/DML.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, email, password, createdAt, updatedAt) VALUES ('Rahul', 'rahul@gmail.com', 'password@123', NOW(), NOW());
+
