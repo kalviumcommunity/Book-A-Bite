425ee2dfea9c6388d76e46979fa11d462e1a81b0
praduman03 <praduman03k@gmail.com>
2023-11-16 07:04:45 +0530

    role based access control and advance sql queries

--- DBMS_Scripts/DDL.sql
+++ DBMS_Scripts/DDL.sql
@@ -38,6 +38,28 @@ ALTER TABLE reservations ADD table_id INT
 
 ALTER TABLE reservations ADD FOREIGN KEY (table_id) REFERENCES tables(table_id);
 
- select * from users where id = 2;
+select * from users where id = 2;
 
 SELECT username, Count(*) AS user_count FROM users GROUP BY username HAVING user_count > 1;  
+
+SELECT reservations.reservation_id, reservations.dateofbook, users.username
+FROM reservations
+INNER JOIN users ON reservations.customer_id = users.id;
+
+SELECT tables.table_id, tables.status, reservations.reservation_id
+FROM tables
+LEFT JOIN reservations ON tables.table_id = reservations.table_id;
+
+SELECT reservations.reservation_id, reservations.dateofbook, tables.table_id
+FROM reservations
+RIGHT JOIN tables ON reservations.table_id = tables.table_id;
+
+CREATE ROLE admin;
+CREATE ROLE waiter;
+CREATE ROLE customer;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON restaurant TO admin, waiter;
+GRANT SELECT ON tables TO waiter, customer;
+GRANT SELECT, INSERT, UPDATE, DELETE ON reservations TO waiter, customer;
+
+GRANT admin TO 'root'@'localhost';
