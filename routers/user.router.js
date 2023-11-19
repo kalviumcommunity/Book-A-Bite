a5af101cc282001212f07f0f77133a2b550cc453
praduman03 <praduman03k@gmail.com>
2023-11-19 10:26:31 +0530

    added changes for abstract class and implemented SRP

--- routers/user.router.js
+++ routers/user.router.js
@@ -2,8 +2,22 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.post("/signup", userController.createUser);
+router.post("/signup", async (req, res) => {
+  try {
+    await userController.createUser(req, res);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
-router.post("/login", userController.loginUser);
+router.post("/login", async (req, res) => {
+  try {
+    await userController.loginUser(req, res);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
